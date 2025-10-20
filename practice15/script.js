d555bb0949de640093ec883403f9ad03faf8c91d
PADA-1-4-03 <pada-1-4-03@Academys-Mac-mini.local>
2025-10-20 17:29:13 +0800

    hicheel13

---
+++ practice15/script.js
@@ -0,0 +1,9 @@
+
+ const name = "Altai";
+ const age = 37;
+ const name2 = "f*cking old"
+
+
+        console.log(typeof name)
+        console.log(typeof age)
+        console.log("my name is " + name + " I'm " + age +" years old " + "yeah I know" + " I'm " + name2)
