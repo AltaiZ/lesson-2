18546d0bf284a6e3e45e7fc87604c6a506da6163
AltaiZ <munkh_altai@yahoo.com>
2025-11-26 21:27:03 +0800

    hicheel 29

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,6 +5,7 @@ import "./App.css";
 import Header from "./Header";
 import Example from "./Example";
 const App = () => {
+  
   return (
     <>
       <Header />
--- vite-project/src/Example.jsx
+++ vite-project/src/Example.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Example = () => {
+  const [count, setCount] = useState (0);
+  const [count1, setCount1] = useState (10);
+  const [count2, setCount2] = useState (100);
   return (
     <div>
     <div className='bg-gray-100 h-[100vh]'>
@@ -10,29 +14,33 @@ const Example = () => {
       <div className='flex justify-center gap-7 pt-10'>
       <div className='flex flex-col gap-3 bg-white p-10 w-90 rounded-[10px] shadow-xl'>
         <p className='font-bold text-xl flex justify-center'>Basic Counter</p>
-        <p className='flex justify-center text-2xl text-blue-600 font-bold '>Count</p>
+        <p className='flex justify-center text-2xl text-blue-600 font-bold '>{count}</p>
         <div className='flex gap-2 justify-center'>
-          <button className='p-4 text-white bg-red-600 rounded-[5px]'>-</button>
-          <button className='p-4 text-white bg-gray-500 rounded-[5px]'>Reset</button>
-          <button className='p-4 text-white bg-green-600 rounded-[5px]'>+</button>
+          <button className='p-4 text-white bg-red-600 rounded-[5px]' onClick={()=>
+            {setCount(count - 1)}
+          }>-</button>
+          <button className='p-4 text-white bg-gray-500 rounded-[5px]' onClick={()=> setCount(0)}>Reset</button>
+          <button className='p-4 text-white bg-green-600 rounded-[5px]' onClick={()=>
+            {setCount(count + 1)}
+          }>+</button>
         </div>
       </div>
        <div className='flex flex-col gap-3 bg-white p-10 w-90 rounded-[10px] shadow-xl'>
         <p className='font-bold text-xl flex justify-center'>Basic Counter</p>
-        <p className='flex justify-center text-2xl text-blue-600 font-bold '>Count</p>
+        <p className='flex justify-center text-2xl text-blue-600 font-bold '>{count1}</p>
         <div className='flex gap-2 justify-center'>
-          <button className='p-4 text-white bg-red-600 rounded-[5px]'>-</button>
-          <button className='p-4 text-white bg-gray-500 rounded-[5px]'>Reset</button>
-          <button className='p-4 text-white bg-green-600 rounded-[5px]'>+</button>
+          <button className='p-4 text-white bg-red-600 rounded-[5px]' onClick={()=> {setCount1(count1 - 5)}}>-</button>
+          <button className='p-4 text-white bg-gray-500 rounded-[5px]' onClick={()=> setCount1(10)}>Reset</button>
+          <button className='p-4 text-white bg-green-600 rounded-[5px]' onClick={()=> {setCount1(count1 + 5)}}>+</button>
         </div>
       </div>
        <div className='flex flex-col gap-3 bg-white p-10 w-90 rounded-[10px] shadow-xl'>
         <p className='font-bold text-xl flex justify-center'>Basic Counter</p>
-        <p className='flex justify-center text-2xl text-blue-600 font-bold '>Count</p>
+        <p className='flex justify-center text-2xl text-blue-600 font-bold '>{count2}</p>
         <div className='flex gap-2 justify-center'>
-          <button className='p-4 text-white bg-red-600 rounded-[5px]'>-</button>
-          <button className='p-4 text-white bg-gray-500 rounded-[5px]'>Reset</button>
-          <button className='p-4 text-white bg-green-600 rounded-[5px]'>+</button>
+          <button className='p-4 text-white bg-red-600 rounded-[5px]' onClick={()=> {setCount2(count2 - 10)}}>-</button>
+          <button className='p-4 text-white bg-gray-500 rounded-[5px]' onClick={()=> setCount2(100)}>Reset</button>
+          <button className='p-4 text-white bg-green-600 rounded-[5px]' onClick={()=> {setCount2(count2 + 10)}}>+</button>
         </div>
       </div>
     </div>
